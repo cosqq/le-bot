e2194e3ba59ea9ce6217c1eaa31c857a0a2c35c6
Clement Ong Shi Qi <clement_ong_sq@aiap.sg>
2024-06-30 00:49:58 +0800

    feat: add debuggers

---
+++ Processer.py
@@ -0,0 +1,14 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+import httpx
+
+class Processer: 
+    async def handle_webhook(self, request:Request):
+        data = await request.json()
+
+        # Ensure PR exists and is opened
+        if "pull_request" in data.keys() and ( data["action"] in ["opened", "reopened"] ):
+            pr = data.get("pull_request")   
+            print (pr)
+
+        return JSONResponse(content={}, status_code=200)
